8d644c29cd55d31181b9f82230e6eda6d97c952b
Arundhati Chakraborty <arun@Arundhatis-MacBook-Pro.local>
2018-08-14 11:40:46 -0700

    initial commit of the online shopping site in Angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAMcxasZlCFhZLYUFwX8enaJHSGexvyliU",
+    authDomain: "oshop-f0c46.firebaseapp.com",
+    databaseURL: "https://oshop-f0c46.firebaseio.com",
+    projectId: "oshop-f0c46",
+    storageBucket: "",
+    messagingSenderId: "292735100393"
+  }
+};
