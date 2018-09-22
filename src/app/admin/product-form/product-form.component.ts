5cfff45c188c46a786cdded1e7cdf26a95bed1cf
Arundhati Chakraborty <arun@Arundhatis-MacBook-Pro.local>
2018-09-21 18:53:59 -0700

    product list

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../category.service';
 import {ProductService} from '../../product.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
@@ -11,13 +12,17 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
     console.log('this.catagories = ' + this.categories$);
   }
 
   save(product) {
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
     console.log(product.valueOf());
   }
 
